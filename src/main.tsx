a8983453bbd06b91cf3fe3e7fa88cfb3d88486f7
chrome350 <chrome350@mail.ru>
2026-08-21 20:36:16 +0300

    Refine calendar layout and extend schedule grid

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import { App } from "./App"
+import "@fontsource/inter/cyrillic-400.css"
+import "@fontsource/inter/cyrillic-500.css"
+import "@fontsource/inter/cyrillic-600.css"
+import "@fontsource/inter/cyrillic-700.css"
 import "./index.css"
 
 createRoot(document.getElementById("root")!).render(
